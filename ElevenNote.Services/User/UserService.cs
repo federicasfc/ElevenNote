4b5480445a187fd143890bb5fb3bec383ba9d841
federicasfc <fesafeco@fakeemail.com>
2022-01-31 15:09:50 -0500

    Built out RegisterUser method in UserService, used that method to create a RegisterUser method in UserController and successfully tested via Postman; also added PasswordHasher to ElevenNote.Service and implemented it in UserService

--- ElevenNote.Services/User/UserService.cs
+++ ElevenNote.Services/User/UserService.cs
@@ -2,11 +2,52 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ElevenNote.Data;
+using ElevenNote.Data.Entities;
+using ElevenNote.Models.User;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 namespace ElevenNote.Services.User
 {
     public class UserService : IUserService
     {
+        private readonly ApplicationDbContext _context;
+        public UserService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<bool> RegisterUserAsync(UserRegister model) //why can we add async here without having it in interface method?
+        {
+            if (await GetUserByEmailAsync(model.Email) != null || await GetUserByUsernameAsync(model.Username) != null)
+                return false; //using the helper methods to ensure that entered username and email are both unique//If they don't return null, they are invalid
 
+            var entity = new UserEntity
+            {
+                Email = model.Email,
+                Username = model.Username,
+                //Password = model.Password,
+                DateCreated = DateTime.Now
+            };
+            var passwordHasher = new PasswordHasher<UserEntity>();
+            entity.Password = passwordHasher.HashPassword(entity, model.Password);
+
+            _context.Users.Add(entity);
+            var numberOfChanges = await _context.SaveChangesAsync();
+
+            return numberOfChanges == 1; //if numOfChanges is equal to 1, return true
+        }
+
+        //Helper Methods 
+        private async Task<UserEntity> GetUserByEmailAsync(string email)
+        {
+            return await _context.Users.FirstOrDefaultAsync(user => user.Email.ToLower() == email.ToLower());
+
+
+        }
+        private async Task<UserEntity> GetUserByUsernameAsync(string username)
+        {
+            return await _context.Users.FirstOrDefaultAsync(user => user.Username.ToLower() == username.ToLower());
+        }
     }
 }
--- ElevenNote.WebAPI/Controllers/UserController.cs
+++ ElevenNote.WebAPI/Controllers/UserController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ElevenNote.Models.User;
 using ElevenNote.Services.User;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -17,7 +18,23 @@ namespace ElevenNote.WebAPI.Controllers
         //Constructor
         public UserController(IUserService service)
         {
-            _service = service;
+            _service = service; //remember service is where the bulk of the logic(and methods reside) that's why we're utilizing it here (creo)
         }
+
+        [HttpPost("Register")]
+        public async Task<IActionResult> RegisterUser([FromBody] UserRegister model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var registerResult = await _service.RegisterUserAsync(model);
+            if (!registerResult)
+            {
+                return BadRequest("User could not be registered");
+            }
+            return Ok("User was registered"); //bringing in service here in the way that previously we would have used context. If I'm following, we are basically adding an extra layer between our database and the userInput data. 
+        } //works, should ask about switched order just in case
     }
 }
